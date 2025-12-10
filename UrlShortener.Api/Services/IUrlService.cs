ffe3a7cf44ef8aea23e6f1abde048579e568f7d3
Phạm Hoàng Anh <anhpham1723@gmail.com>
2025-12-11 04:13:36 +0700

    Initial commit

--- UrlShortener.Api/Services/IUrlService.cs
+++
@@ -1,7 +0,0 @@
-﻿using UrlShortener.Api.Models;
-namespace UrlShortener.Api.Services;
-public interface IUrlService
-{
-    Task<UrlMapping> CreateAsync(string originalUrl);
-    Task<UrlMapping?> GetAsync(string shortCode);
-}
---
+++ UrlShortener/Services/IUrlService.cs
@@ -0,0 +1,7 @@
+﻿using UrlShortener.Models;
+namespace UrlShortener.Services;
+public interface IUrlService
+{
+    Task<UrlMapping> CreateAsync(string originalUrl);
+    Task<UrlMapping?> GetAsync(string shortCode);
+}
